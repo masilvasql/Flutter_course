5c722ab60baa356f7777de3d6f0e494575267d22
Marcelo Abrão da Silva <masilvasql@gmail.com>
2022-04-17 18:17:50 -0300

    iniciando com widgets

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
-main(){
-  runApp(PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget{
+
+  @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Text('Olá Flutter!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá Flutter'),
+      ),
     );
   }
 }
