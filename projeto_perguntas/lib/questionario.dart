71cf54520860a15760c2e9c52625646a90a1bd7d
Marcelo Abrão <masilvasql@gmail.com>
2022-04-28 20:08:22 -0300

    criado função de pontuação

--- projeto_perguntas/lib/questionario.dart
+++ projeto_perguntas/lib/questionario.dart
@@ -5,7 +5,7 @@ import 'resposta.dart';
 class Questionario extends StatelessWidget {
   final List<Map<String, Object>> perguntas;
   final int perguntaSelecionada;
-  final void Function() responder;
+  final void Function(int) responder;
 
   const Questionario({
     Key? key,
@@ -20,16 +20,19 @@ class Questionario extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
         ? perguntas[perguntaSelecionada].cast()['respostas']
         : [];
 
     return Column(
       children: <Widget>[
         Questao(texto: perguntas[perguntaSelecionada]['texto'].toString()),
-        ...respostas
-            .map((t) => Resposta(texto: t, quandoSelecionado: responder))
-            .toList(),
+        ...respostas.map((resp) {
+          return Resposta(
+              texto: resp['texto'].toString(),
+              quandoSelecionado: () =>
+                  responder(int.parse(resp['pontuacao'].toString())));
+        }).toList(),
       ],
     );
   }
