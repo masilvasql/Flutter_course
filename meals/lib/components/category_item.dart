93c2a3b58a6d000a55a0ea67847bc31dd9826e77
Marcelo Abrão <masilvasql@gmail.com>
2022-05-19 21:07:04 -0300

    adicionado fontes, registrado fontes, criado um tema para a aplicação

--- meals/lib/components/category_item.dart
+++ meals/lib/components/category_item.dart
@@ -13,7 +13,10 @@ class CategortItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(15),
-      child: Text(category.title),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(15),
           gradient: LinearGradient(colors: [
--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -8,10 +8,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData(
+      fontFamily: "Raleway",
+      canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+    );
     return MaterialApp(
       title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          secondary: Colors.amber,
+          primary: Colors.pink,
+        ),
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 20,
+          ),
+        ),
       ),
       home: CategoriesScreen(),
     );
